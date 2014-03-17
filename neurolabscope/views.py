d3d560f5e02564c486b5487da8d34aa1df7ad691
Samuel Garcia <sam.garcia.die@gmail.com>
2014-03-17 15:24:19 +0100

    TriggerredOscilloscope

--- neurolabscope/views.py
+++ neurolabscope/views.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from collections import OrderedDict
 
-from pyacq.gui import Oscilloscope, OscilloscopeDigital, TimeFreq
+from pyacq.gui import Oscilloscope, OscilloscopeDigital, TimeFreq, TriggeredOscilloscope
 
-views_list = [Oscilloscope, OscilloscopeDigital, TimeFreq ]
+views_list = [Oscilloscope, OscilloscopeDigital, TimeFreq, TriggeredOscilloscope ]
 views_dict = OrderedDict()
 for v in views_list:
     views_dict[v.__name__] = v
 
 subdevice_to_view = {
-    'AnalogInput': ['Oscilloscope', 'TimeFreq'],
+    'AnalogInput': ['Oscilloscope', 'TriggeredOscilloscope', 'TimeFreq'],
     'DigitalInput':  ['OscilloscopeDigital'],
     'Event' : [ ],
 }
